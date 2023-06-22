d6018fefacfa9bea253a3e28b13fd7c55418f804
BlazingWarlord <66896016+BlazingWarlord@users.noreply.github.com>
2023-06-22 14:19:41 +0530

    Update OS_Checker.py
    Changed Command

--- OS_Checker.py
+++ OS_Checker.py
@@ -2,27 +2,23 @@ import platform
 import subprocess
 
 def get_server_os(ip_address):
-    try:
-        output = subprocess.check_output(['ping', '-c', '1', ip_address])
-        if platform.system() == 'Windows':
-            ttl = int(output.split(b'TTL=')[1].split()[0])
-        elif platform.system() == 'Linux':
-            ttl = int(output.split(b'ttl=')[1].split()[0])
-        else:
-            return 'Unknown'
+    output = subprocess.check_output(['ping',ip_address])
+    if platform.system() == 'Windows':
+        ttl = int(output.split(b'TTL=')[1].split()[0])
+    elif platform.system() == 'Linux':
+        ttl = int(output.split(b'ttl=')[1].split()[0])
+    else:
+        return 'UnknowN'
 
-        if ttl <= 64:
-            return 'Linux'
-        elif ttl <= 128:
-            return 'Windows'
-        elif ttl <= 255:
-            return 'Unix'
+    if ttl <= 64:
+        return 'Linux'
+    elif ttl <= 128:
+        return 'Windows'
+    elif ttl <= 255:
+        return 'Unix'
 
-    except subprocess.CalledProcessError:
-        return 'Unknown'
 
-# Example usage
-target_ip = '23.45.160.244'  # Replace with the target IP address
+target_ip = '10.32.15.51' #IP address
 
 os_name = get_server_os(target_ip)
 print(f"Operating System: {os_name}")
