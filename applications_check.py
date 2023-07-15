a711e8131d6e40cff7a8b3239ff4d5d7dea26e71
BlazingWarlord <66896016+BlazingWarlord@users.noreply.github.com>
2023-07-16 00:17:43 +0530

    Update applications_check.py

--- applications_check.py
+++ applications_check.py
@@ -3,15 +3,24 @@ import subprocess
 
 def self_assess():
     cmd="powershell -Command [System.Environment]::OSVersion.VersionString"
-    os=subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    os=subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout
 
     cmd = "powershell -Command Get-Process"  
-    proc = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    proc = subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout
 
     command = "Get-NetTCPConnection | Select-Object LocalAddress,LocalPort,RemoteAddress,RemotePort,State"
-    open_ports = subprocess.run(['powershell.exe', '-Command', command], capture_output=True, text=True)
-    
-    return [os,proc,open_ports]
+    open_ports = subprocess.run(['powershell.exe', '-Command', command], capture_output=True, text=True).stdout
+
+
+    splitted = open_ports.split('\n')
+    print(splitted)
+    open_ = []
+    for port in splitted:
+        if 'LocalPort' in port:
+            open_.append(port.split(':')[1])
+        
+
+    return [os,proc,open_]
 
 def connection(hostname,username,password):
 
@@ -58,6 +67,7 @@ running_applications = ["web", "database"]
 open_ports = ["80", "443"]
 
 l = connection("localhost","11110","Blazing123")
+
 
 predicted_server_type = predict_server_type(l[1], l[2])
 print("Predicted Server Type:", predicted_server_type)
